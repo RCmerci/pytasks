125dd0543d53efd9f21cd0a62e55089dd79acc14
RCmerci <562837353@qq.com>
2014-08-27 14:47:14 +0800

    =

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name="pytasks",
+    version="1.0",
+    packages=["pytasks",],
+    license="BSD",
+    author="rcmerci",
+    description="work on time-triggered or event-triggered tasks",
+)
